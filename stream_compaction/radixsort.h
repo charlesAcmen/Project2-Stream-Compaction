ad9765b51562c1b4932215a94323750444223751
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-06-29 18:03:53 +0800

    kernScatter for radix sort

--- stream_compaction/radixsort.h
+++ stream_compaction/radixsort.h
@@ -11,8 +11,11 @@ namespace StreamCompaction {
         //if bit b of idata[idx] == 0,then bools[idx] = 1; otherwise false;
         __global__ void kernMapToBoolean(int n, int *bools, const int *idata);
         //perform an exclusive scan on bools
-        __global__ void kernExclusiveScan(int n,int* odata,const int* bools);
-        
+        __global__ void kernExclusiveScan(int n,int* scanned,const int* bools);
+        //scatter the elements in idata into odata, using bools to determine 
+        //whether an element should be scattered, and using scanned to determine 
+        //the location to scatter to.
+        __global__ void kernScatter(int n,int *odata, const int *idata, const int *bools, const int *scanned);
         //sort array with non-negative elements in an ascending order
         void radixsort(int n, int *data);
     }
