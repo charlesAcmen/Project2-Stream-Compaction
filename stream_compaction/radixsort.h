cab46cc874dda06c9323501fef6603528f78d65b
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-06-29 18:00:52 +0800

    kernExclusiveScan for radix sort

--- stream_compaction/radixsort.h
+++ stream_compaction/radixsort.h
@@ -10,7 +10,8 @@ namespace StreamCompaction {
         //map the input array to an array of 0s and 1s,That is, for each element in idata,
         //if bit b of idata[idx] == 0,then bools[idx] = 1; otherwise false;
         __global__ void kernMapToBoolean(int n, int *bools, const int *idata);
-        // __global__ void kern
+        //perform an exclusive scan on bools
+        __global__ void kernExclusiveScan(int n,int* odata,const int* bools);
         
         //sort array with non-negative elements in an ascending order
         void radixsort(int n, int *data);
